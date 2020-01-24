fa7b911a3f60d364d863957a02dd1974c6ae4b6a
Alex Castillo <alex@castillo.io>
2020-01-24 13:30:08 -0500

    RFR: login & calm pages

---
+++ src/pages/Calm.js
@@ -0,0 +1,39 @@
+import React, { useState, useEffect } from "react";
+import useWindowSize from "react-use/lib/useWindowSize";
+import Confetti from "react-confetti";
+import { Nav } from "../components/Nav";
+
+export function Calm({ user, notion }) {
+  const [calmScore, setCalmScore] = useState();
+  const [showConfetti, setShowConfetti] = useState(false);
+  const { width, height } = useWindowSize();
+
+  useEffect(() => {
+    if (!user || !notion) {
+      return;
+    }
+
+    const subscription = notion.calm().subscribe(calm => {
+      const score = Number(calm.probability.toFixed(2));
+      setCalmScore(score);
+      if (score > 0.5) {
+        setShowConfetti(true);
+      } else {
+        setShowConfetti(false);
+      }
+    });
+
+    return () => {
+      subscription.unsubscribe();
+    };
+  }, [user, notion]);
+
+  return (
+    <main>
+      <Nav user={user} />
+      <h1>Confetti!</h1>
+      <h2>Calm score: {calmScore}</h2>
+      {showConfetti ? <Confetti width={width} height={height} /> : null}
+    </main>
+  );
+}
---
+++ src/pages/Login.js
@@ -0,0 +1,51 @@
+import React, { useState, useEffect } from "react";
+import { LoginForm } from "../components/LoginForm";
+
+export function Login({ notion, user, setUser, setDeviceId }) {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
+  const [isLoggingIn, setIsLoggingIn] = useState(false);
+
+  useEffect(() => {
+    if (!user && notion && email && password) {
+      login();
+    }
+
+    async function login() {
+      setIsLoggingIn(true);
+      const auth = await notion
+        .login({ email, password })
+        .catch(error => {
+          setError(error.message);
+        });
+
+      if (auth) {
+        setUser(auth.user);
+        resetForm();
+      }
+      setIsLoggingIn(false);
+    }
+  }, [email, password, notion, user, setUser, setError]);
+
+  function onLogin({ email, password, deviceId }) {
+    if (email && password && deviceId) {
+      setError("");
+      setEmail(email);
+      setPassword(password);
+      setDeviceId(deviceId);
+    } else {
+      setError("Please fill the form");
+    }
+  }
+
+  function resetForm() {
+    setError("");
+    setEmail("");
+    setPassword("");
+  }
+
+  return (
+    <LoginForm onLogin={onLogin} error={error} loading={isLoggingIn} />
+  );
+}
