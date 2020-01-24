ae88986aa0ab48b8a95140930b6998b77c779968
Alex Castillo <alex@castillo.io>
2020-01-24 13:30:59 -0500

    RFR: handle auth at the app level

--- src/App.js
+++ src/App.js
@@ -1,22 +1,73 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Router, navigate } from "@reach/router";
+import { Notion } from "@neurosity/notion";
+import useLocalStorage from "react-use/lib/useLocalStorage";
 
-import { Login } from "./components/Login/Login";
-import { Confetti } from "./components/Confetti/Confetti";
+import { Login } from "./pages/Login";
+import { Logout } from "./pages/Logout";
+import { Calm } from "./pages/Calm";
 
-function App() {
-  function onLogin(loginData) {
-    navigate("/confetti", {
-      state: loginData
+export function App() {
+  const [notion, setNotion] = useState(null);
+  const [user, setUser] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [deviceId, setDeviceId] = useLocalStorage("deviceId");
+
+  useEffect(() => {
+    if (user) {
+      navigate("/calm");
+    }
+  }, [user]);
+
+  useEffect(() => {
+    if (deviceId) {
+      const notion = new Notion({ deviceId });
+      setNotion(notion);
+    } else {
+      setLoading(false);
+    }
+  }, [deviceId]);
+
+  useEffect(() => {
+    if (!notion) {
+      return;
+    }
+
+    const subscription = notion.onAuthStateChanged().subscribe(user => {
+      if (user) {
+        setUser(user);
+      } else {
+        navigate("/");
+      }
+      setLoading(false);
     });
+
+    return () => {
+      subscription.unsubscribe();
+    };
+  }, [notion]);
+
+  function resetState() {
+    setNotion(null);
+    setUser(null);
+    setDeviceId("");
+  }
+
+  if (loading) {
+    return <div>Loading...</div>;
   }
 
   return (
     <Router>
-      <Login path="/" onLogin={onLogin} />
-      <Confetti path="/confetti" />
+      <Login
+        path="/"
+        notion={notion}
+        user={user}
+        setUser={setUser}
+        setDeviceId={setDeviceId}
+      />
+      <Calm path="/calm" notion={notion} user={user} />
+      <Logout path="/logout" notion={notion} resetState={resetState} />
     </Router>
   );
 }
-
-export default App;
--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { render } from "react-dom";
-import App from "./App";
+import { App } from "./App";
 import "./global.css";
 
 render(<App />, document.getElementById("root"));
