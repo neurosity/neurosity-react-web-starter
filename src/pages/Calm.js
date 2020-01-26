b14ee0627763aad8210e98a1f0ac5b0c6cf93f83
Alex Castillo <alex@castillo.io>
2020-01-26 17:01:56 -0500

    FEAT: add meter for calm progress

--- src/pages/Calm.js
+++ src/pages/Calm.js
@@ -2,11 +2,8 @@ import React, { useState, useEffect } from "react";
 import { Nav } from "../components/Nav";
 import { Ocean } from "../components/Ocean/Ocean";
 
-const testSlider = true;
-
 export function Calm({ user, notion }) {
   const [calm, setCalm] = useState(0);
-  console.log("calm", calm);
 
   useEffect(() => {
     if (!user || !notion) {
@@ -24,26 +21,9 @@ export function Calm({ user, notion }) {
 
   return (
     <main>
-      {testSlider ? <TestSlider calm={calm} setCalm={setCalm} /> : null}
       {user ? <Nav notion={notion} /> : null}
+      <meter value={calm} min={0} max={1} />
       <Ocean calm={calm} setCalm={setCalm} />
     </main>
-  );
-}
-
-function TestSlider({ calm, setCalm }) {
-  const style = { position: "absolute", zIndex: 3, padding: "20px" };
-  return (
-    <div style={style}>
-      <input
-        type="range"
-        min={0}
-        max={1}
-        value={calm}
-        step={0.01}
-        onChange={e => setCalm(Number(e.target.value))}
-      />
-      <small>&nbsp;{calm}% Calm</small>
-    </div>
   );
 }
